fed0b868d86076ddc5f4bb5afdb5a3184f5b30de
dayanFreitas <dayan.freitas@catolicasc.org.br>
2018-12-03 02:44:29 -0200

    04-adicionado funcao cadastro

--- ProgramacaoI/ProjetoAlunos/alunos.c
+++ ProgramacaoI/ProjetoAlunos/alunos.c
@@ -1,32 +1,64 @@
 #include <stdio.h>
 #include <ctype.h>
+#define maxAlunos 4
 
 void println(char *texto);
+struct alunos{
+    int id;
+    char nome[20];
+};
 
 int main(int argc, char const *argv[])
 {
-    struct alunos{
-        int id;
-        char nome[20];
-    };
-    struct alunos aluno;
-    
+    struct alunos aluno[maxAlunos];
     //Declaração de funções 
     void menu();
     void lerOperecao(int *operacao);
     void verificarOperacao(int *operacao);
+    void cadastrarAluno(struct alunos t[],int *numeroDeAlunos);
 
+    int numeroDeAlunos = 0;
     int operacao;
-    int *pOp;
+    int *pOp,*pNumAlunos;
     pOp = &operacao;
-   
-    menu();
-    lerOperecao(pOp);
-    verificarOperacao(pOp);
+    pNumAlunos = &numeroDeAlunos;
 
+    do{ 
+        menu();
+        lerOperecao(pOp);
+        printf("Numero de alunos cadastrados%d\n",numeroDeAlunos);
+        switch(*pOp)
+        {
+            case 1:
+                printf("--Cadastras Alunos--\n");
+                cadastrarAluno(aluno,pNumAlunos);                
+                break;
+            case 2:
+                printf("AINDA SEM FUNÇÃO\nCHAMADO 05");
+                break;
+        }
+        for(int i=0;i < maxAlunos;i++){
+            if(aluno[i].id != 0){
+                printf("--alunos cadastrados--\n");
+                printf("Id:%d\n",aluno[i].id);
+                printf("Nome:%s\n",aluno[i].nome);
+            }
+        }
+    }while(*pOp != 0);
     return 0;
+  
 }
 //Funções 
+void cadastrarAluno(struct alunos t[],int *numeroDeAlunos){ 
+    if(*numeroDeAlunos < maxAlunos){
+        fflush(stdin);
+        t[*numeroDeAlunos].id = *numeroDeAlunos+1;
+        printf("Digite o nome:");
+        gets(t[*numeroDeAlunos].nome);
+        *numeroDeAlunos+=1;
+    }
+
+}
 void menu(){
     println("1 - Cadastrar aluno");
     println("2 - Ver alunos cadastrados");
