3d67a2e81c3e874a755211455be2bfaa2f469bdf
dayanFreitas <dayan.freitas@catolicasc.org.br>
2018-12-04 15:44:07 -0200

    Atualizado

--- ProgramacaoI/ProjetoAlunos/funcoes.h
+++ ProgramacaoI/ProjetoAlunos/funcoes.h
@@ -6,16 +6,27 @@ struct alunos{
     int id;
     char nome[20];
 };
+int existsStudent(struct alunos t[],int id);
 
 void lerOperecao(int *operacao);
 void limparPosicao(struct alunos t[],int posicao);
-void mostraPosicao(struct alunos t[],int posicao);
+void mostraPosicao(struct alunos t[],int posicao,int respostaExists);
 void zerarVariaveis(struct alunos t[],int quantidade); 
 
 void verificarOperacao(int *operacao,struct alunos t[],int *numeroDeAlunos,int maxAlunos);
 void cadastrarAluno(struct alunos t[],int *numeroDeAlunos,int maxAlunos);
 void verAlunos(struct alunos t[],int *numeroDeAlunos);
-void buscarAluno(struct alunos t[]);   
+void buscarAluno(struct alunos t[],int maxAlunos);   
+
+int existsStudent(struct alunos t[],int id){
+    if(t[id].id == -1)
+    {
+        return -1;
+    }
+    else{
+        return 1;
+    }
+}
 
 void lerOperecao(int *operacao){
     int op;
@@ -24,16 +35,20 @@ void lerOperecao(int *operacao){
     *operacao=op;
 }
 void limparPosicao(struct alunos t[],int posicao){
-    t[posicao].id = 0;
+    t[posicao].id = -1;
     strcpy(t[posicao].nome," ");
 }
 
-void mostraPosicao(struct alunos t[],int posicao){
-    printf("Id:%d",t[posicao].id);
-    printf("\n");
-    printf("Nome:%s",t[posicao].nome);
-    printf("\n");
-    
+void mostraPosicao(struct alunos t[],int posicao,int respostaExists){
+    if(respostaExists == -1)
+    {
+        printf("ID not registered\n");
+    }else if(respostaExists == 1){
+        printf("Id:%d",t[posicao].id);
+        printf("\n");
+        printf("Nome:%s",t[posicao].nome);
+        printf("\n");
+    }
 }
 
 void zerarVariaveis(struct alunos t[],int quantidade){
@@ -48,7 +63,7 @@ void verAlunos(struct alunos t[],int *numeroDeAlunos){
     { 
         for(int i = 0; i < *numeroDeAlunos;i++)
         {
-        mostraPosicao(t,i);
+        mostraPosicao(t,i,existsStudent(t,i));
         }
     }
     else if(*numeroDeAlunos == 0)
@@ -58,11 +73,13 @@ void verAlunos(struct alunos t[],int *numeroDeAlunos){
     pause();
 }
 
-void buscarAluno(struct alunos t[]){
+void buscarAluno(struct alunos t[],int maxAlunos){
     int id;
-    printf("Digite o id:");
-    scanf("%d",&id);
-    mostraPosicao(t,id);
+    do{
+        printf("Digite o id:");
+        scanf("%d",&id);
+    }while(id > maxAlunos);
+    mostraPosicao(t,id,existsStudent(t,id));
     pause();
 }
 void cadastrarAluno(struct alunos t[],int *numeroDeAlunos,int maxAlunos){ 
@@ -97,7 +114,7 @@ void verificarOperacao(int *operacao,struct alunos t[],int *numeroDeAlunos,int m
         case 3:
             printf("\n");
             printf("--Busca de alunos--\n");
-            buscarAluno(t);
+            buscarAluno(t,maxAlunos);
             break;
     }
 }
